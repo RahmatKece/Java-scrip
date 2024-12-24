c1965c4182d3bdeb18cf063455755d4f582b20b1
RahmatKece <rahmatullahmuhamad45@gmail.com>
2024-12-24 20:09:27 +0700

    Add files via upload

---
+++ TUGAS KECIL/main.js
@@ -0,0 +1,34 @@
+/*let saldoAwal = 25000;   // kalau tugas yang ini udah selesaii
+
+alert("Saldo awal anda adalah" + saldoAwal);
+
+let saldoTopup = prompt("Masukkan nominal yang ingin Anda topup:");
+saldoTopup = Number(saldoTopup);
+const saldoAkhir = saldoAwal + saldoTopup;
+alert(
+  `Anda berhasil topup sebesar${saldoTopup} dan sekarang uang ada sebesar ${saldoAkhir}`
+);
+*/
+
+//TUGAS YANG INI BELUM SELESAI
+
+let Hari = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+let hari = new Date().getDay();
+console.log("Hari ini adalah hari" + Hari[hari]);
+
+// YANG INI JUGA UDAH PAHAM
+
+/*let umur = prompt("Masukan Umur Anda");
+
+if (umur >= 35) {
+  umur = "Kamu Bapak Bapak";
+} else if (umur <= 34) {
+  umur = "Kamu Itu masih remaja";
+} else if (umur >= 17) {
+  umur = "Kamu Masih pelajar";
+} else {
+  umur = "Kamu itu dibawah Umurr pergi sanaa";
+}
+
+console.log(` Jadi kamu itu termasuk golongan ${umur}`);
+*/
